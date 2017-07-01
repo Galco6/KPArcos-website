8548649202a41b7b8afce8bde21ec6e149ea7ee0
Víctor <vicgalco@hotmail.com>
2017-07-01 12:28:16 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <ul class="mdl-mini-footer__link-list">
       <li><a href="http://www.clubpiraguismoarcos.es/contacto">Contacto</a></li>
       <li><a href="http://www.clubpiraguismoarcos.es/wp-login.php">Acceso</a></li>
-      <li>Desarrolado por <a href="#">Víctor Galvín</a></li>
+      <li>Desarrollado por <a href="https://github.com/Galco6">Víctor Galvín</a></li>
     </ul>
   </div>
 </div>
